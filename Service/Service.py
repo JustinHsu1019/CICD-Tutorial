51852051e893b21b195b6543657737c5801dd902
Justin.Hsu <justin.hsu@chainsea.com.tw>
2024-04-05 00:33:08 +0800

    Update root return

--- Service/Service.py
+++ Service/Service.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def main_update():
-    return "Service is ready USUSUS GGDD"
+    return "Service is ready"
 
 @app.route('/git_update', methods=['POST'])
 def git_update():
