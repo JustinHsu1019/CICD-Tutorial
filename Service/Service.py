47f99edf7df6d83929f3a06ee7aa8cb58c484435
Justin.Hsu <justin.hsu@chainsea.com.tw>
2024-04-05 01:13:38 +0800

    test verify

--- Service/Service.py
+++ Service/Service.py
@@ -9,7 +9,28 @@ app = Flask(__name__)
 
 @app.route('/')
 def main_update():
-    return "Service is ready XDDDDDG"
+    return "Service is ready JUSTIN"
+
+def verify_signature(payload_body, secret_token, signature_header):
+    """驗證從GitHub發送的請求，確認其SHA256簽名的有效性。
+
+    如果未經授權，則拋出403錯誤。
+
+    Args:
+        payload_body: 請求的原始體，用於驗證（request.data）。
+        secret_token: GitHub應用的webhook秘鑰。
+        signature_header: 從GitHub收到的x-hub-signature-256頭信息。
+    """
+    if not signature_header:
+        abort(403, "X-Hub-Signature-256頭信息缺失！")
+
+    signature = signature_header.split('=')[1]
+
+    hmac_gen = hmac.new(secret_token.encode('utf-8'), payload_body, hashlib.sha256)
+    expected_signature = "sha256=" + hmac_gen.hexdigest()
+
+    if not hmac.compare_digest(expected_signature, signature):
+        abort(403, "請求簽名不匹配！")
 
 @app.route('/git_update', methods=['POST'])
 def git_update():
@@ -21,16 +42,7 @@ def git_update():
     secret_token = config['pythonanywhere']['github_secret'].encode('utf-8')
 
     signature_header = request.headers.get('X-Hub-Signature-256')
-    if not signature_header:
-        abort(400, 'Missing X-Hub-Signature-256 header')
-
-    signature = signature_header.split('=')[1]
-
-    hmac_gen = hmac.new(secret_token, request.data, hashlib.sha256)
-    expected_signature = hmac_gen.hexdigest()
-
-    if not hmac.compare_digest(expected_signature, signature):
-        abort(403)
+    verify_signature(request.data, secret_token, signature_header)
 
     pythonanywhere_username = config['pythonanywhere']['username']
     pythonanywhere_api_token = config['pythonanywhere']['api_token']
@@ -52,7 +64,7 @@ def git_update():
     if response.status_code == 200:
         return "Updated Service from Git and reloaded PythonAnywhere web app", 200
     else:
-        return f'Failed to Updated Service from Git and reload PythonAnywhere web app. Status code: {response.status_code}', 500
+        return f'Failed to update Service from Git and reload PythonAnywhere web app. Status code: {response.status_code}', 500
 
 if __name__ == '__main__':
     app.run(threaded=True, debug=True)
