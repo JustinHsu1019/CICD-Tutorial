0f1eec0e5b21ece24c3823a31bf324675b0ab91c
Justin.Hsu <justin.hsu@chainsea.com.tw>
2024-04-05 00:04:53 +0800

    Update Service

--- Service/Service.py
+++ Service/Service.py
@@ -1,10 +1,20 @@
 from flask import Flask
+import git
 
 app = Flask(__name__)
 
 @app.route('/')
-def git_update():
+def main_update():
     return "Service is ready"
+
+@app.route('/git_update', methods=['POST'])
+def git_update():
+    repo = git.Repo('./CICD-First-Try')
+    origin = repo.remotes.origin
+    repo.create_head('master', origin.refs.master).set_tracking_branch(origin.refs.master).checkout()
+    origin.pull()
+    return "Pulled", 200
 
 if __name__ == '__main__':
     app.run(debug=True)
+# 0405
