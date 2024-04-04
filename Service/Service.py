5060ce390634a1f016d142ab98c9548642f14b0e
Justin.Hsu <justin.hsu@chainsea.com.tw>
2024-04-05 01:24:23 +0800

    Update CICD Script

--- Service/Service.py
+++ Service/Service.py
@@ -2,35 +2,12 @@ from flask import Flask, request, abort
 import git
 import requests
 import configparser
-import hmac
-import hashlib
 
 app = Flask(__name__)
 
 @app.route('/')
 def main_update():
-    return "Service is ready JUSTTEST"
-
-def verify_signature(payload_body, secret_token, signature_header):
-    """驗證從GitHub發送的請求，確認其SHA256簽名的有效性。
-
-    如果未經授權，則拋出403錯誤。
-
-    Args:
-        payload_body: 請求的原始體，用於驗證（request.data）。
-        secret_token: GitHub應用的webhook秘鑰。
-        signature_header: 從GitHub收到的x-hub-signature-256頭信息。
-    """
-    if not signature_header:
-        abort(403, "X-Hub-Signature-256頭信息缺失！")
-
-    signature = signature_header.split('=')[1]
-
-    hmac_gen = hmac.new(secret_token, payload_body, hashlib.sha256)
-    expected_signature = "sha256=" + hmac_gen.hexdigest()
-
-    if not hmac.compare_digest(expected_signature, signature):
-        abort(403, "請求簽名不匹配！")
+    return "Service is ready XDXDXDXD"
 
 @app.route('/git_update', methods=['POST'])
 def git_update():
@@ -39,10 +16,9 @@ def git_update():
     config = configparser.ConfigParser()
     config.read(config_path)
 
-    secret_token = config['pythonanywhere']['github_secret'].encode('utf-8')
-
-    signature_header = request.headers.get('X-Hub-Signature-256')
-    verify_signature(request.data, secret_token, signature_header)
+    token_header = request.headers.get('Authorization')
+    if not token_header or not token_header.startswith("Token "):
+        abort(403, "未提供有效的 API Token")
 
     pythonanywhere_username = config['pythonanywhere']['username']
     pythonanywhere_api_token = config['pythonanywhere']['api_token']
